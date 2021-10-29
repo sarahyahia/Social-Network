9b782d4bb7a791485ff2f3dd9562433c10328bbe
Sarah <sarah.ali2810@gmail.com>
2021-10-30 01:35:51 +0200

    user search

--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileView, ProfileEditView, AddFollower, RemoveFollower, Like, Dislike, PostView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
+from .views import UserSearch, ProfileView, ProfileEditView, AddFollower, RemoveFollower, Like, Dislike, PostView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
 
 
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
-    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower')
+    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+    path('search/', UserSearch.as_view(), name='profile-search'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
@@ -224,3 +225,17 @@ class Dislike(LoginRequiredMixin, View):
             post.dislikes.remove(request.user)
         next = request.POST.get('next','/')
         return HttpResponseRedirect(next)
+
+
+
+class UserSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get('query')
+        profile_list = UserProfile.objects.filter(
+            Q(user__username__icontains=query)
+        )
+        context ={
+            'profile_list': profile_list,
+        }
+        return render(request, 'social/search.html', context)
+    
